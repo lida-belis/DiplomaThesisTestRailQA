008f3eca295b2aa116833b2c9774e15dcebd8542
maryia.bartosh <maryia.bartosh@vizor-games.com>
2020-08-22 17:39:36 +0300

    add package on Test, MilestonesEditPage, MilestonesPage

---
+++ src/test/java/pages/MilestonesPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+
+import baseEntity.BasePageFactory;
+import core.BrowsersService;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MilestonesPage extends BasePageFactory {
+
+    // Проверка, что milestones с названием 'MilestoneDiploma' отобразился на странице
+    @FindBy(xpath = "//a[text()='MilestoneDiploma']")
+    public WebElement stonesNameField;
+
+    // Нажатие на кнопку Edit
+    @FindBy(xpath = "//a[text()='Edit']")
+    public WebElement editStonesButton;
+
+
+    public MilestonesPage(BrowsersService browsersService) {
+        super(browsersService, false);
+    }
+
+    @Override
+    protected void openPage() {
+
+    }
+
+    @Override
+    public boolean isPageOpened() {
+        return false;
+    }
+}
