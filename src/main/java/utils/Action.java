39f1640ce04517f1d23d930e2d9ebb430d6846dc
maryia.bartosh <maryia.bartosh@vizor-games.com>
2020-08-30 16:15:15 +0300

    add actions

---
+++ src/main/java/utils/Action.java
@@ -0,0 +1,17 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Action {
+    private WebDriver driver;
+
+    public Action(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void upMessage(WebElement webElement){
+        Action action = new Action(driver);
+        action.
+    }
+}
